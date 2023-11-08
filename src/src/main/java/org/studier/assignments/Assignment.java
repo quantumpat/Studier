7736e23892dae138c176f507020a599eaed69bd5
quantumpat <patrickjcarroll17@gmail.com>
2023-11-07 21:56:55 -0700

    Base configuration for Assignment.java & Question.java!

--- src/src/main/java/org/studier/assignments/Assignment.java
+++ src/src/main/java/org/studier/assignments/Assignment.java
@@ -1,5 +1,25 @@
 package org.studier.assignments;
 
+import java.util.ArrayList;
+
+/**
+ * A new assignment to be completed.
+ *
+ * @author Patrick Carroll & Evan Alexander
+ * @version November 7th, 2023
+ */
 public class Assignment {
-    public int i;
+
+    /**
+     * All the questions in the assignment.
+     */
+    private ArrayList<Question> questions;
+
+    /**
+     * Constructs a new assignment.
+     */
+    public Assignment() {
+
+    }
+
 }
---
+++ src/src/main/java/org/studier/assignments/Question.java
@@ -0,0 +1,28 @@
+package org.studier.assignments;
+
+/**
+ * A question to be completed.
+ *
+ * @author Patrick Carroll & Evan Alexander
+ * @version November 7th, 2023
+ */
+public class Question {
+
+    /**
+     * The question being asked on canvas.
+     */
+    private String questionText;
+
+    /**
+     * The answer to the question.
+     */
+    private String questionAnswer;
+
+    /**
+     * Constructs a new question.
+     */
+    public Question() {
+
+    }
+
+}
